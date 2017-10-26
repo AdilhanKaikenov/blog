3c0fb408db2e7d710e876fb4d8e55b3f8bfa0f95
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2017-10-26 13:30:38 +0600

    BlogCategoryID.java equals & hashCode methods added

--- blog-core/src/main/java/com/epam/adok/core/entity/BlogCategoryID.java
+++ blog-core/src/main/java/com/epam/adok/core/entity/BlogCategoryID.java
@@ -29,4 +29,22 @@ public class BlogCategoryID implements Serializable {
     public void setCategory(Category category) {
         this.category = category;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BlogCategoryID that = (BlogCategoryID) o;
+
+        if (blog != null ? !blog.equals(that.blog) : that.blog != null) return false;
+        return category != null ? category.equals(that.category) : that.category == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = blog != null ? blog.hashCode() : 0;
+        result = 31 * result + (category != null ? category.hashCode() : 0);
+        return result;
+    }
 }
