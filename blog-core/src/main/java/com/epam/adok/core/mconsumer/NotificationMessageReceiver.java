221502b6d217e5b0261f0bffcdd654ad3b1f1091
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2017-11-13 14:52:35 +0600

    NotificationMessageReceiver (initial implementation)

--- blog-core/src/main/java/com/epam/adok/core/mconsumer/NotificationMessageReceiver.java
+++ blog-core/src/main/java/com/epam/adok/core/mconsumer/NotificationMessageReceiver.java
@@ -1,14 +1,18 @@
 package com.epam.adok.core.mconsumer;
 
+import com.epam.adok.core.entity.Notification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.io.Serializable;
 
 @MessageDriven(
         name = "NotificationMessageReceiver",
@@ -18,9 +22,9 @@ import javax.persistence.PersistenceContext;
         activationConfig =
                 {
                         @ActivationConfigProperty(
-                        propertyName = "destination",
-                        propertyValue = "java:/jms/queue/NotificationsQueue"
-                ),
+                                propertyName = "destination",
+                                propertyValue = "java:/jms/queue/NotificationsQueue"
+                        ),
                         @ActivationConfigProperty(
                                 propertyName = "destinationType",
                                 propertyValue = "javax.jms.Queue"
@@ -35,6 +39,25 @@ public class NotificationMessageReceiver implements MessageListener {
 
     @Override
     public void onMessage(Message message) {
+        try {
+            if (!(message instanceof ObjectMessage)) {
+                log.error("Message should be instance of ObjectMessage class");
+                return;
+            }
+            ObjectMessage objectMessage = (ObjectMessage) message;
+            Serializable object = objectMessage.getObject();
+            if (!(object instanceof Notification)) {
+                log.error("Not expected instance in message");
+                return;
+            }
 
+            Notification notification = (Notification) object;
+
+            entityManager.persist(notification);
+
+        } catch (JMSException e) {
+            e.printStackTrace(); // TODO:
+        }
     }
 }
+
