5d4090bf4fcbe9437c74ebb6447e0976d7fcc65b
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2017-11-20 15:52:15 +0600

    ValueAnnotationProducer.java updated

--- blog-core/src/main/java/com/epam/adok/core/annotation/value/ValueAnnotationProducer.java
+++ blog-core/src/main/java/com/epam/adok/core/annotation/value/ValueAnnotationProducer.java
@@ -2,12 +2,15 @@ package com.epam.adok.core.annotation.value;
 
 import com.epam.adok.core.exception.PropertyManagerException;
 import com.epam.adok.core.propmanager.PropertiesManager;
+import com.epam.adok.core.util.converter.StringConverter;
+import com.epam.adok.core.util.converter.StringConvertersHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import java.lang.reflect.Type;
 
 @ApplicationScoped
 public class ValueAnnotationProducer {
@@ -16,8 +19,42 @@ public class ValueAnnotationProducer {
 
     @Produces
     @Value
-    public String produceValue(InjectionPoint injectionPoint) throws PropertyManagerException {
+    public Integer produceIntegerValue(InjectionPoint injectionPoint) throws PropertyManagerException {
+        return (Integer) produceValue(injectionPoint);
+    }
+
+    @Produces
+    @Value
+    public Long produceLongValue(InjectionPoint injectionPoint) throws PropertyManagerException {
+        return (Long) produceValue(injectionPoint);
+    }
+
+    @Produces
+    @Value
+    public String produceStringValue(InjectionPoint injectionPoint) throws PropertyManagerException {
+        return getStringValue(injectionPoint);
+    }
+
+    private Object produceValue(InjectionPoint injectionPoint) throws PropertyManagerException {
         log.info("Entering produceValue() method.");
+
+        String value = getStringValue(injectionPoint);
+
+        Type type = injectionPoint.getType();
+        log.info("Injection type : {}", type);
+
+        StringConverter<?> converter = StringConvertersHolder.getTheRightConverter(type);
+
+        Object convertedValue = converter.convert(value);
+
+        log.info("Converted Value : {}", convertedValue);
+
+        log.info("Leaving produceValue() method. Value : {}", value);
+        return convertedValue;
+    }
+
+    private String getStringValue(InjectionPoint injectionPoint) throws PropertyManagerException {
+        log.info("Entering getStringValue() method.");
 
         Value annotation = injectionPoint.getAnnotated().getAnnotation(Value.class);
 
@@ -29,8 +66,8 @@ public class ValueAnnotationProducer {
 
         if (value == null) {
             log.info("Key {} in {} file was not found.", key, propertiesSource);
+            return null;
         }
-        log.info("Leaving produceValue() method. Value : {}", value);
         return value;
     }
 }
